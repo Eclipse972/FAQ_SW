63c20cfb8dd77ccf61d3539a402314d4b4ae8af5
Eclipse972 <christophe.hervi@gmail.com>
2018-12-29 07:23:12 +0100

    premier commit

---
+++ les articles.mm
@@ -0,0 +1,305 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Les articles&#xa;de la&#xa;FAQ SolidWorks" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1542498788136"><hook NAME="MapStyle" zoom="0.91">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<node TEXT="Accueil" POSITION="left" ID="ID_420715855" CREATED="1526380485219" MODIFIED="1528384208721" HGAP="50" VSHIFT="-50" STYLE="bubble">
+<edge COLOR="#ff0000" WIDTH="3"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="pr&#xe9;sentation du site" ID="ID_1568678834" CREATED="1526380814962" MODIFIED="1528383148319" STYLE="fork">
+<node TEXT="Pourquoi ?" ID="ID_600070468" CREATED="1528386131228" MODIFIED="1542233375246"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ce site n'a pas pour vocation de remplacer l'aide de SW mais de r&#233;pondre aux questionS r&#233;currentes de mes &#233;l&#232;ves.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="la barre d&apos;onglets" ID="ID_1924442455" CREATED="1528385726113" MODIFIED="1528385745562" STYLE="fork"/>
+<node TEXT="le menu" ID="ID_1421295796" CREATED="1528385755410" MODIFIED="1528385763491"/>
+<node TEXT="l&apos;article" ID="ID_1836013899" CREATED="1528385815466" MODIFIED="1528385826613"/>
+<node TEXT="articles connexes" ID="ID_335950066" CREATED="1528385764692" MODIFIED="1528385773616"/>
+</node>
+<node TEXT="moi" ID="ID_140275635" CREATED="1526380804402" MODIFIED="1528386052972" STYLE="fork"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Utile?
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="qui suis-je?" ID="ID_1223195307" CREATED="1528385690006" MODIFIED="1528385961259"/>
+<node TEXT="mon site DT" ID="ID_1773794977" CREATED="1526380807986" MODIFIED="1528383148312" STYLE="fork"/>
+</node>
+<node TEXT="liste des mots cl&#xe9;s" ID="ID_1842849095" CREATED="1528385641878" MODIFIED="1528385680261" STYLE="fork"/>
+<node TEXT="nouveaut&#xe9;s" ID="ID_253617544" CREATED="1526380890500" MODIFIED="1528383148299" STYLE="fork">
+<node TEXT="articles mis &#xe0; jour" ID="ID_1823348878" CREATED="1526380917270" MODIFIED="1526380924596"/>
+<node TEXT="article ajout&#xe9;" ID="ID_349198835" CREATED="1542233424171" MODIFIED="1542233430713"/>
+<node TEXT="moteur de recherche?" ID="ID_1511110366" CREATED="1526380924997" MODIFIED="1526380930467"/>
+</node>
+</node>
+<node TEXT="Pi&#xe8;ce" POSITION="left" ID="ID_141785098" CREATED="1526380491828" MODIFIED="1528383129160" STYLE="bubble">
+<edge COLOR="#0000ff" WIDTH="3"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="esquisse" ID="ID_1924496097" CREATED="1526675534157" MODIFIED="1528384514473" STYLE="fork">
+<node TEXT="dessiner" ID="ID_1430719732" CREATED="1526678165535" MODIFIED="1528383171261" STYLE="fork"/>
+<node TEXT="coter une esquisse" ID="ID_390488448" CREATED="1526380776449" MODIFIED="1528383171262" STYLE="fork"/>
+<node TEXT="utilisation des contraintes g&#xe9;om&#xe9;triques" ID="ID_8774979" CREATED="1526380750368" MODIFIED="1528383171259" STYLE="fork"/>
+</node>
+<node TEXT="fonctions" ID="ID_1272760423" CREATED="1526675547708" MODIFIED="1528384514470" STYLE="fork">
+<node TEXT="ajout et enl&#xe8;vement de mati&#xe8;re par r&#xe9;volution" ID="ID_1253846029" CREATED="1526380566286" MODIFIED="1528385074772" STYLE="fork">
+<node TEXT="sph&#xe8;re" ID="ID_34801564" CREATED="1526380625862" MODIFIED="1526380630185"/>
+<node TEXT="tronc de c&#xf4;ne" ID="ID_142713536" CREATED="1526380630950" MODIFIED="1526687904190"/>
+<node TEXT="cylindre" ID="ID_211439176" CREATED="1526380643897" MODIFIED="1526380647909"/>
+<node TEXT="pi&#xe8;ces de r&#xe9;volution" ID="ID_1825796774" CREATED="1526380648536" MODIFIED="1526380655847">
+<node TEXT="l&apos;esquisse" ID="ID_1002237315" CREATED="1528384904167" MODIFIED="1528385025547"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      cr&#233;ation de l'axe
+    </p>
+    <p>
+      cr&#233;ation de l'esquisse: contour ferm&#233; ou avec fermeture automatique
+    </p>
+    <p>
+      importance de l'axe pour lacotation
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="r&#xe9;volution" ID="ID_526768767" CREATED="1528384927468" MODIFIED="1528384935837"/>
+</node>
+<node TEXT="pi&#xe8;ce faite en tounage" ID="ID_408522505" CREATED="1542232890291" MODIFIED="1542232966156"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sp&#233;cifiquement pour les TU
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="ajout et enl&#xe8;vement par extrusion" ID="ID_1630166481" CREATED="1526380560781" MODIFIED="1542232961577" STYLE="fork">
+<node TEXT="cylindre" ID="ID_1226196671" CREATED="1526380589601" MODIFIED="1526380599553"/>
+<node TEXT="prisme" ID="ID_720558490" CREATED="1526380600178" MODIFIED="1526380605169"/>
+<node TEXT="cas g&#xe9;n&#xe9;ral" ID="ID_1635227147" CREATED="1526380605907" MODIFIED="1526380616306"/>
+<node TEXT="pi&#xe8;ce faite en fraisage" ID="ID_334203316" CREATED="1542232969848" MODIFIED="1542232985214"/>
+</node>
+<node TEXT="assitance pour le per&#xe7;age" ID="ID_374008902" CREATED="1526676572185" MODIFIED="1528383171247" STYLE="fork">
+<node TEXT="per&#xe7;age" ID="ID_1536480622" CREATED="1526380695774" MODIFIED="1526676614335"/>
+<node TEXT="trou taraud&#xe9;" ID="ID_683315570" CREATED="1526380878261" MODIFIED="1526380888854"/>
+</node>
+<node TEXT="sym&#xe9;trie" ID="ID_220502340" CREATED="1526380719455" MODIFIED="1528383171252" STYLE="fork"/>
+<node TEXT="r&#xe9;p&#xe9;tition" ID="ID_811982235" CREATED="1526380728175" MODIFIED="1528383171257" STYLE="fork"/>
+<node TEXT="cong&#xe9; et chanfrein" ID="ID_1722356949" CREATED="1526380741104" MODIFIED="1528383171211" STYLE="fork"/>
+</node>
+<node TEXT="couper la pi&#xe8;ce" ID="ID_474692404" CREATED="1528385122794" MODIFIED="1528385139798" STYLE="fork">
+<node TEXT="suivant plan de r&#xe9;f&#xe9;rence" ID="ID_695886437" CREATED="1528385277008" MODIFIED="1528385296926"/>
+<node TEXT="plan parall&#xe8;le" ID="ID_1968116438" CREATED="1528385319188" MODIFIED="1528385332078"/>
+<node TEXT="suivant un plan cr&#xe9;&#xe9;" ID="ID_1580376245" CREATED="1528385334786" MODIFIED="1528385384105"/>
+</node>
+<node TEXT="exploiter l&apos;arbre de cr&#xe9;ation" ID="ID_1946037529" CREATED="1528384465150" MODIFIED="1528384496781" STYLE="fork"/>
+</node>
+<node TEXT="Assemblage" POSITION="right" ID="ID_944297772" CREATED="1526380524664" MODIFIED="1528384202793" STYLE="bubble" VSHIFT="-30">
+<edge COLOR="#ff00ff" WIDTH="3"/>
+<node TEXT="exploiter l&apos;arbre de cr&#xe9;ation" ID="ID_938334082" CREATED="1528383343210" MODIFIED="1528383898099" STYLE="fork">
+<node TEXT="arbre -&gt; zone graphique" ID="ID_1169259879" CREATED="1528383962593" MODIFIED="1528383982227"/>
+<node TEXT="zone graphique -&gt; arbre" ID="ID_1181142758" CREATED="1528383982895" MODIFIED="1528384012469"/>
+<node TEXT="ouvrir un sous ensemble/composant" ID="ID_1522560487" CREATED="1528384015802" MODIFIED="1528384034731"/>
+<node TEXT="cacher/monter un composant" ID="ID_736639294" CREATED="1528383317073" MODIFIED="1528384078529" STYLE="fork"/>
+</node>
+<node TEXT="ajouter des contraintes" ID="ID_655026047" CREATED="1526381130588" MODIFIED="1528383171181" STYLE="fork">
+<node TEXT="appui plan" ID="ID_1129550598" CREATED="1528383207203" MODIFIED="1528383217777"/>
+<node TEXT="coaxialit&#xe9;" ID="ID_1221237526" CREATED="1528383219094" MODIFIED="1528384322538"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Que ce ce soit un centrage court ou long ils sont r&#233;alis&#233;s de la m&#234;me mani&#232;re
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="contraintes limites" ID="ID_1344669755" CREATED="1531019156463" MODIFIED="1531019352634"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      elles permettent de rendre le fonctinement conforme &#224; la r&#233;alit&#233;
+    </p>
+    <p>
+      exmple1; le v&#233;rin, comment faire pour le pisont ne puisse plus sortir de la chambre en utilisant une limite lin&#233;aire
+    </p>
+    <p>
+      exmple 2 la vanne sph&#233;rique comment faire pour que la manette ne d&#233;passe pas le 1/4 en uilisant une limite circulaire
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="utiliser les configurations" ID="ID_1865529424" CREATED="1531019489817" MODIFIED="1531019518312" STYLE="fork"/>
+<node TEXT="faire un &#xe9;clat&#xe9;" ID="ID_1027413262" CREATED="1531019562191" MODIFIED="1531019595937" STYLE="fork"/>
+<node TEXT="faire un &#xe9;corch&#xe9;" ID="ID_818859278" CREATED="1531019694351" MODIFIED="1531019767702" STYLE="fork"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      en ne coupant que certaine pi&#232;ces
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Mise en plan" POSITION="left" ID="ID_1383385457" CREATED="1526380509111" MODIFIED="1528383129182" STYLE="bubble">
+<edge COLOR="#00ff00" WIDTH="3"/>
+<node TEXT="choisir un fond de plan" ID="ID_481879308" CREATED="1526380997353" MODIFIED="1528384159453" STYLE="fork"/>
+<node TEXT="les vues" ID="ID_1881392975" CREATED="1526676172592" MODIFIED="1528384159455" STYLE="fork">
+<node TEXT="palette de vues" ID="ID_1533769219" CREATED="1526676912106" MODIFIED="1526676926762"/>
+<node TEXT="vue projet&#xe9;e" ID="ID_949879126" CREATED="1526380989399" MODIFIED="1526380996550"/>
+<node TEXT="vue en coupe simple" ID="ID_711652150" CREATED="1526380954391" MODIFIED="1526380964967">
+<node TEXT="avec 2012" ID="ID_1384721864" CREATED="1526380964972" MODIFIED="1526380972885"/>
+<node TEXT="avec 2015" ID="ID_1111396579" CREATED="1526380973352" MODIFIED="1526380977349"/>
+</node>
+<node TEXT="mise en page" ID="ID_1036880012" CREATED="1526381025610" MODIFIED="1526381032420">
+<node TEXT="d&#xe9;placer les vues" ID="ID_157904754" CREATED="1526381032424" MODIFIED="1526677562082"/>
+<node TEXT="modifier l&apos;&#xe9;chelle" ID="ID_496530960" CREATED="1526381042282" MODIFIED="1542233044725"/>
+</node>
+<node TEXT="ins&#xe9;rer une perspective" ID="ID_722323352" CREATED="1526381061820" MODIFIED="1526381091197">
+<node TEXT="standard" ID="ID_1664917681" CREATED="1526381091201" MODIFIED="1526381094890"/>
+<node TEXT="personalis&#xe9;e" ID="ID_620547263" CREATED="1526381095837" MODIFIED="1526381108217"/>
+</node>
+</node>
+<node TEXT="la cotation" ID="ID_683564504" CREATED="1526676301616" MODIFIED="1528384159458" STYLE="fork">
+<node TEXT="ins&#xe9;rer la cotation" ID="ID_982518148" CREATED="1526381006777" MODIFIED="1526675916637"/>
+<node TEXT="forcer le sens des fl&#xe8;ches" ID="ID_190542604" CREATED="1526676335565" MODIFIED="1526676473948"/>
+<node TEXT="nombre de d&#xe9;cimales" ID="ID_1315602142" CREATED="1528385456761" MODIFIED="1528385480533" STYLE="fork"/>
+<node TEXT="modifier les lignes de rappel" ID="ID_723380356" CREATED="1528385179132" MODIFIED="1528385198831"/>
+</node>
+<node TEXT="remplir le cartouche" ID="ID_369796455" CREATED="1526381017832" MODIFIED="1528385248037" STYLE="fork"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Les cartouche que je propose sont pr&#233;-remplis.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="exporter en pdf" ID="ID_795405089" CREATED="1526381110797" MODIFIED="1528384159448" STYLE="fork"/>
+<node TEXT="exploiter l&apos;arbre de cr&#xe9;ation" ID="ID_1589909495" CREATED="1528384551665" MODIFIED="1528384579218" STYLE="fork"/>
+<node TEXT="m&#xe9;canique graphique dans une mise en plan" ID="ID_634321178" CREATED="1526381154767" MODIFIED="1528383171190" STYLE="fork">
+<node TEXT="statique" ID="ID_108074444" CREATED="1526381179298" MODIFIED="1526381183037"/>
+<node TEXT="cin&#xe9;matique" ID="ID_3348707" CREATED="1526381183536" MODIFIED="1526381193229"/>
+</node>
+</node>
+<node TEXT="Autre" POSITION="right" ID="ID_1489485974" CREATED="1526380533721" MODIFIED="1528384668064" STYLE="bubble">
+<edge COLOR="#00ffff" WIDTH="3"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ici les article qui concernent les trois module
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="zoom" ID="ID_1775133551" CREATED="1526381199615" MODIFIED="1528383171196" STYLE="fork">
+<node TEXT="zoom au mieux" ID="ID_1704450225" CREATED="1528384674168" MODIFIED="1528384679589"/>
+<node TEXT="zoom fen&#xea;tre" ID="ID_1068169596" CREATED="1528384679853" MODIFIED="1528384685930"/>
+<node TEXT="ajuster le zoom" ID="ID_1251606975" CREATED="1528384701317" MODIFIED="1528384725067"/>
+<node TEXT="d&#xe9;placer la vue" ID="ID_1685330396" CREATED="1528385984541" MODIFIED="1528385995474"/>
+</node>
+<node TEXT="ouvrir un fichier efficacement" ID="ID_1371388257" CREATED="1526381268129" MODIFIED="1528383171170" STYLE="fork"/>
+</node>
+</node>
+</map>
