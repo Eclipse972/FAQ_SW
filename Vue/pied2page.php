f92a13bc526341202ba24305b1232c941a9d5ec3
Eclipse972 <christophe.hervi@gmail.com>
2019-01-13 01:15:53 +0100

    MAJ

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,3 +1,3 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
 <?php if ($MODE == 'FAQ')	echo ' - <a href="?f=1">Me contacter</a>'; ?>
- - derni&egrave;re mise à jour: 2 janvier 2019</p>
+ - derni&egrave;re mise à jour: 13 janvier 2019</p>
