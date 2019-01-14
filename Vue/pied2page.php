ff5f170a5e90a9aefe0058b91449967fcc8bcd9f
Eclipse972 <christophe.hervi@gmail.com>
2019-01-15 00:06:15 +0100

    ajout d'un lien vers mes DT en ligne

--- Vue/pied2page.php
+++ Vue/pied2page.php
@@ -1,3 +1,4 @@
 <p>Site optimis&eacute; pour <img src="Vue/images/chrome.png" alt="Chrome"> et <img src="Vue/images/firefox.png" alt="Firefox">
 <?php if ($MODE == 'FAQ')	echo ' - <a href="?f=1">Me contacter</a>'; ?>
- - derni&egrave;re mise à jour: 13 janvier 2019</p>
+ - <a target="_blank" href="http://dossiers.techniques.free.fr">Mes dossiers techniques en ligne</a>
+ - derni&egrave;re mise à jour: 15 janvier 2019</p>
