97d5a1e5d6f3f029f0150324dbfc1674fac4afed
ChristopHe <christophe.hervi@free.fr>
2021-04-10 03:43:22 +0200

    liste des URL réécrites de toutes les pages du site

---
+++ BD/Vue_URLvalides.sql
@@ -0,0 +1,14 @@
+DROP VIEW IF EXISTS Vue_URLvalides;
+CREATE VIEW Vue_URLvalides AS
+SELECT
+	alpha AS niveau1,
+	beta AS niveau2,
+	gamma AS niveau3,
+	(SELECT CONCAT('/',ptiNom) FROM Items WHERE alpha = (SELECT niveau1) AND beta = (SELECT niveau2) AND gamma = 0 AND beta>0) AS texte2,
+	(SELECT CONCAT('/',ptiNom) FROM Items WHERE alpha = (SELECT niveau1) AND beta = (SELECT niveau2) AND gamma = (SELECT niveau3) AND beta>0 AND gamma>0) AS texte3,
+	CONCAT('/',(SELECT ptiNom FROM Items WHERE alpha = (SELECT niveau1) AND beta = 0 AND gamma = 0)
+		,IF(ISNULL((SELECT texte2)),'',(SELECT texte2))
+		,IF(ISNULL((SELECT texte3)),'',(SELECT texte3))
+	) AS URL
+FROM Items
+ORDER BY niveau1 ASC, niveau2 ASC, niveau3 ASC
