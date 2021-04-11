5074dd0f0541058ef313fd01ad68570c09af07a4
ChristopHe <christophe.hervi@free.fr>
2021-04-11 14:46:39 +0200

    séparationde PEUNC du reste du code

---
+++ PEUNC/classes/Page.php
@@ -0,0 +1,127 @@
+<?php
+// classes pages de PEUNC
+abstract class Page {
+
+	protected $BD;
+	protected $logo;
+	protected $titre;
+
+	abstract public function CSS();
+	abstract public function Section();
+	abstract public function PagesConnexes();	// page en lien sur le site
+
+	public function __construct() {
+		$this->BD = new base2donnees;
+		$this->logo = "Vue/images/logo.png";
+		$this->titre = "Foire Aux Questions SolidWorks de ChristopHe";
+	}
+
+	public function CodeCSS($nom)	{ echo "<link rel=\"stylesheet\" href=\"Vue/{$nom}.css\" />\n"; }
+
+	public function Logo() { echo $this->logo; }
+
+	public function Titre() { echo $this->titre; }
+
+	public function LienFormulaire() { echo " - <a href=\"?alpha=-2\">Me contacter</a>\n"; }
+
+	public function Onglets() {
+		$T_Onglets = $this->BD->Liste_onglets();
+		echo "<ul>\n";
+		foreach($T_Onglets as $alpha => $code)
+			echo "\t\t<li>", (($alpha == $_SESSION['alpha']) ? str_replace('href', 'id="onglet_actif" href', $code) : $code), "</li>\n";
+		echo "\t</ul>\n";
+	}
+
+	public function Menu() {
+		$T_item = $this->BD->Liste_items();
+		echo "<nav>\n\t<ul>\n";
+		foreach($T_item as $beta => $code) {
+			echo "\t\t<li>", (($beta == $_SESSION['beta']) ? str_replace('href', 'id="item_actif" href', $code) : $code), "</li>\n";
+			if ($beta == $_SESSION['beta']) {	// sous-menu?
+				$T_sous_item = $this->BD->Liste_sous_items();
+				if (isset($T_sous_item)) {	// génération sous-menu s'il existe
+					echo "\t\t<ul>\n";
+					foreach($T_sous_item as $gamma => $sous_code)
+						echo "\t\t\t<li>", ($gamma == $_SESSION['gamma']) ? str_replace('href', 'id="sous_item_actif" href', $sous_code) : $sous_code, "</li>\n";
+					echo "\t\t</ul>\n";
+				}
+			}
+		}
+		echo "\t</ul>\n</nav>\n";
+	}
+
+	public function ArticlesConnexes() {
+		echo "<aside>\n";
+		$this->PagesConnexes();
+		echo "</aside>\n";
+	}
+}
+
+// Classes filles
+class PageErreur extends Page {
+
+	public function __construct() {
+		parent::__construct();
+		$_SESSION['alpha'] = -1;	// aucun alpha sélectionné
+	}
+	// fonctions obligatoires
+	public function CSS() { $this->CodeCSS("erreur"); }
+
+	public function Menu() { echo "<nav></nav>"; }
+
+	public function PagesConnexes() {}
+
+	public function Section() {
+?>
+	<h1>Erreur <?=$_SESSION['beta']?>: <?=$this->BD->TexteErreur()?></h1>
+	<p>S&eacute;lectionnez un des onglets en haut de cette page.</p>
+	<p>Si le probl&egrave;me persiste envoyez-moi un courriel en <a href="faq.sw@free.fr">cliquant ici</a>.</p>
+<?php
+	}
+	// fin de fonctions obligatoires
+}
+
+class PageFormulaire extends Page {
+	// la valeur du paramètre formulaire n'a aucune incidence car elle n'est pas lue
+
+	public function __construct() {
+		parent::__construct();
+		if (empty($_POST)) { // préparation affichage du formulaire
+
+		} else {	// traitement du formulaire
+
+		}
+	}
+	// fonctions obligatoires
+	public function CSS() { $this->CodeCSS("formulaire"); }
+
+	public function Menu() { echo "<nav></nav>"; }
+
+	public function PagesConnexes() {}
+
+	public function Section() {
+?>
+	<h1>Formulaire en construction</h1>
+	<form method="post" action="?formulaire=1" id=formulaire>
+		<p>Nom : <input 	 type="text" name="nom" /></p>
+		<p>Courriel : <input type="email" name="courriel" /></p>
+		<p>Objet : <input	 type="text" name="objet" /></p>
+		<p>Message : <textarea name="message" rows="6"></textarea></p>
+		<div id=validation>
+			<p>Validation du formulaire</p>
+			<ul>
+			<?php $this->Afficher_validation();?>
+			</ul>
+			<p>Code	<input type="text" name="code" style="width:100px;" /></p>
+		</div>
+		<p style="text-align:center;">
+			<input type="submit" value="Envoyer" style="width:100px; margin-right:200px" />
+		</p>
+	</form>
+<?php
+	}
+	// fin de fonctions obligatoires
+	public function Afficher_validation() {}
+
+	public function LienFormulaire() { echo ""; }
+}
