c618c13d85561867e7a4e6d31ddadb97cefcd6d5
ChristopHe <christophe.hervi@free.fr>
2021-04-11 15:07:26 +0200

    suppression de la classe PageAccueil inutile dans la BDD

--- BD/ClassePage.sql
+++ BD/ClassePage.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: faq.sw.sql.free.fr
--- Généré le : Sam 10 Avril 2021 à 01:58
+-- Généré le : Dim 11 Avril 2021 à 15:06
 -- Version du serveur: 5.0.83
 -- Version de PHP: 5.3.9
 
@@ -39,7 +39,6 @@ CREATE TABLE IF NOT EXISTS `ClassePage` (
 INSERT INTO `ClassePage` (`ID`, `nom`, `description`) VALUES
 (1, 'PageArticle', 'Article d''une page'),
 (2, 'PageVE', 'article sur les volumes élémentaires'),
-(0, 'PageAccueil', 'Page sur laquelle on arrive lors de l''absence de paramètre'),
 (-1, 'PageErreur', 'Page d''erreur du site'),
 (-2, 'PageFormulaire', 'Formulaire de contact'),
 (-3, 'PageMembre', 'gestion des membres');
