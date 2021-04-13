91f3dcb6d71a3ba5305e6ea8b5646e6b210cb4ef
ChristopHe <christophe.hervi@free.fr>
2021-04-13 12:46:34 +0200

    descripton du script

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <?php
-/***********************************************************************************************************************************
-	contrôleur principal
-************************************************************************************************************************************/
-// PEUNC
-require 'PEUNC/classes/Page.php';
-require 'PEUNC/classes/BDD.php';
-// fin PEUNC
+require 'PEUNC/classes/Page.php';	require 'PEUNC/classes/BDD.php'; // utilisation de PEUNC
 
 require 'Modele/classe_BD.php';
 require 'Modele/classe_traceur.php';
 require 'Modele/classe_Page.php';
 
 session_start();
-/* contexte sauvegardé dans la session
- * alpha		0 => alpha accueil
- * beta			0 => pas d'beta sélectionné
- * gamma	0 => pas de sous-beta sélectionné
- *
- * fonctionnalité à venir: article de plusieurs pages
- * page;		0 => page unique
- * */
+/* contexte sauvegardé dans la session (alpha, beta, gamma) par importance décroissante
+	(X;0;0) => page de 1er niveau.
+ 		X=0 -> page d'accueil.
+ 		Si X<0 on tombe sur les pages spéciales de PEUNC
+ 		(-2;0;0) formulaire de contact
+	(X;Y;0) avec Y>0 => page de 2e niveau
+		(-1;code;0) -> page d'erreur avec son code
+	(X;Y;Z) avec Z>0 => page de 3e niveau
+	Toute autre configuration provoque une erreur 404
+*/
 $TRACEUR = new Traceur; // voir avant dernière ligne pour affichage du rapport
 $T_paramètresURL = array('alpha', 'beta', 'gamma');	// paramètres principaux
 
@@ -28,8 +24,8 @@ foreach($T_paramètresURL as $valeur)								// récupération des paramètres
 
 $BD = new base2donnees;
 $classePage = $BD->ClassePage();
-if (isset($classePage))
-	$PAGE = new $classePage;
-else header("location:?alpha=-1&beta=404");
+if (!		isset($classePage))	die('Classe page non d&eacute;finie dans la BBD.');
+if (!class_exists($classePage))	die('La classe page demand&eacute; n&apos;existe pas.');
+$PAGE = new $classePage;
 
 include"Vue/doctype.html";	// utilise les méthodes de l'objet page
