0fbc9542d4e71e39b61d23c960cc101878a20e32
ChristopHe <christophe.hervi@free.fr>
2021-04-18 00:42:23 +0200

    URL complête dans la table connexes et suppression du champ type devenu inutile

--- BD/Connexes.sql
+++ BD/Connexes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: faq.sw.sql.free.fr
--- Généré le : Dim 18 Avril 2021 à 00:22
+-- Généré le : Dim 18 Avril 2021 à 00:40
 -- Version du serveur: 5.0.83
 -- Version de PHP: 5.3.9
 
@@ -28,8 +28,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `Connexes` (
   `squelette_ID` int(11) NOT NULL,
   `texte` varchar(99) collate latin1_general_ci NOT NULL,
-  `URL` varchar(99) collate latin1_general_ci NOT NULL,
-  `type` tinyint(1) NOT NULL default '1' COMMENT '0: interne, 1: SW, 2:autre',
+  `URL` varchar(255) collate latin1_general_ci NOT NULL,
   UNIQUE KEY `articles_connexes` (`texte`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
@@ -37,25 +36,25 @@ CREATE TABLE IF NOT EXISTS `Connexes` (
 -- Contenu de la table `Connexes`
 --
 
-INSERT INTO `Connexes` (`squelette_ID`, `texte`, `URL`, `type`) VALUES
-(39, 'barre d&apos;outils', 'sldworks/r_sketch_toolbar.htm', 1),
-(15, 'Barre d''outils Fonctions', 'sldworks/r_features_toolbar.htm', 1),
-(54, 'ligne de construction', 'sldworks/c_centerlines.htm', 1),
-(53, 'Icônes des relations d''esquisse', 'sldworks/c_sketch_relations_icons.htm', 1),
-(44, 'Extrusions', 'sldworks/Hidd_dve_end_spec_dlg.htm', 1),
-(45, 'Révolutions', 'sldworks/c_Revolves_Folder.htm', 1),
-(46, 'Balayages', 'sldworks/c_Sweeps_Folder.htm', 1),
-(47, 'Fonction de symétrie', 'sldworks/c_Mirror_Feature_Overview.htm', 1),
-(48, 'Le PropertyManager Répétition linéaire', 'sldworks/HIDD_LPATTERN.htm', 1),
-(49, 'Le PropertyManager Répétition circulaire', 'sldworks/HIDD_CPATTERN.htm', 1),
-(50, 'Présentation de l''Assistance pour le perçage', 'sldworks/c_Hole_Wizard_Overview.htm', 1),
-(51, 'Vue d''ensemble des congés', 'sldworks/c_Fillet_Overview.htm', 1),
-(51, 'Création d''une fonction de chanfrein', 'sldworks/HIDD_DVE_FEAT_CHAMFER.htm', 1),
-(57, 'Vues en coupe dans les modèles', 'sldworks/t_section_views_models.htm', 1),
-(63, 'Créer un cylindre par révolution', '?alpha=1&beta=5&gamma=3', 0),
-(67, 'Créer un cylindre par extrusion', '?alpha=1&beta=5&gamma=2', 0),
-(65, 'Créer un tronc de cône par extrusion', '?alpha=1&beta=5&gamma=6', 0),
-(68, 'Créer un tronc de cône par r&eacute;volution', '?alpha=1&beta=5&gamma=5', 0);
+INSERT INTO `Connexes` (`squelette_ID`, `texte`, `URL`) VALUES
+(39, 'barre d&apos;outils', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/r_sketch_toolbar.htm'),
+(15, 'Barre d''outils Fonctions', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/r_features_toolbar.htm'),
+(54, 'ligne de construction', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_centerlines.htm'),
+(53, 'Icônes des relations d''esquisse', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_sketch_relations_icons.htm'),
+(44, 'Extrusions', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/Hidd_dve_end_spec_dlg.htm'),
+(45, 'Révolutions', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_Revolves_Folder.htm'),
+(46, 'Balayages', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_Sweeps_Folder.htm'),
+(47, 'Fonction de symétrie', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_Mirror_Feature_Overview.htm'),
+(48, 'Le PropertyManager Répétition linéaire', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/HIDD_LPATTERN.htm'),
+(49, 'Le PropertyManager Répétition circulaire', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/HIDD_CPATTERN.htm'),
+(50, 'Présentation de l''Assistance pour le perçage', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_Hole_Wizard_Overview.htm'),
+(51, 'Vue d''ensemble des congés', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/c_Fillet_Overview.htm'),
+(51, 'Création d''une fonction de chanfrein', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/HIDD_DVE_FEAT_CHAMFER.htm'),
+(57, 'Vues en coupe dans les modèles', 'http://help.solidworks.com/2015/french/SolidWorks/sldworks/t_section_views_models.htm'),
+(63, 'Créer un cylindre par révolution', '?alpha=1&beta=5&gamma=3'),
+(67, 'Créer un cylindre par extrusion', '?alpha=1&beta=5&gamma=2'),
+(65, 'Créer un tronc de cône par extrusion', '?alpha=1&beta=5&gamma=6'),
+(68, 'Créer un tronc de cône par r&eacute;volution', '?alpha=1&beta=5&gamma=5');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
