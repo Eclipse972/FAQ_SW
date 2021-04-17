271587c8b9c69530ccd29f592fc78a523ddc826d
ChristopHe <christophe.hervi@free.fr>
2021-04-18 00:49:04 +0200

    simplification du code de la vue

--- BD/Vue_pagesConnexes.sql
+++ BD/Vue_pagesConnexes.sql
@@ -4,20 +4,7 @@ SELECT
 	alpha,
 	beta,
 	gamma,
-	CONCAT(
-		'<a href="',
-		IF (Connexes.type = 1,'http://help.solidworks.com/2015/french/SolidWorks/',''),
-		URL,
-		'" title="',
-		CASE Connexes.type
-			WHEN 0 THEN 'Lien interne'
-			WHEN 1 THEN 'Aide de SolidWorks 2015 en ligne'
-			ELSE 'Autre type de lien'
-		END,
-		'" ',
-		IF (Connexes.type = 0,'','target="_blank"'),
-		'>',Connexes.texte,'</a>'
-	) AS URL
+	CONCAT('<a href="',URL,'" target="_blank">',Connexes.texte,'</a>') AS URL
 FROM Squelette
 INNER JOIN Connexes ON Squelette.ID = Connexes.Squelette_ID
-ORDER BY alpha ASC, beta ASC, gamma ASC, type ASC
+ORDER BY alpha ASC, beta ASC, gamma ASC
