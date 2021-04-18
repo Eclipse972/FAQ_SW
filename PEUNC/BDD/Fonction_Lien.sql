ba1812fe8e7a75f7b3129ae270d21d1b566d5f8c
ChristopHe <christophe.hervi@free.fr>
2021-04-18 19:19:29 +0200

    nouvelle version de la fonction lien

--- PEUNC/BDD/Fonction_Lien.sql
+++ PEUNC/BDD/Fonction_Lien.sql
@@ -4,4 +4,8 @@ CREATE FUNCTION Lien(texte TEXT, alpha INT, beta INT, gamma INT) RETURNS TEXT
 RETURN CONCAT(	'<a href="?alpha=',CAST(alpha AS CHAR),
 				IF(beta > 0,CONCAT('&beta=',CAST(beta AS CHAR)),''),
 				IF((beta > 0 AND gamma > 0),CONCAT('&gamma=',CAST(gamma AS CHAR)),''), #-- permet de ne pas imbriquer les CONCAT
-				'">',texte,'</a>')
+				'">',texte,'</a>');
+
+DROP FUNCTION IF EXISTS Lien2;
+CREATE FUNCTION Lien2(texte TEXT, alpha INT, beta INT, gamma INT) RETURNS TEXT
+RETURN CONCAT(	'<a href="',(SELECT URL FROM Vue_URLvalides WHERE niveau1 = alpha AND niveau2 = beta AND niveau3 = gamma),'">',texte,'</a>')
