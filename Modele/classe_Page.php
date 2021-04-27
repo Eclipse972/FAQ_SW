7bbafe3f61d1ac24bff3591a99f0b55b3ba4e1d8
ChristopHe <christophe.hervi@free.fr>
2021-04-27 16:16:47 +0200

    Déplacement du dossier images à la racine Ainsi dans le code source de la page, on ne voit que images/fichier. Le code source de la page étant public, je cache ainsi la structure sous-jacente du site

--- Modele/classe_Page.php
+++ Modele/classe_Page.php
@@ -33,7 +33,7 @@ class PageVE extends PageArticle	{
 		?>
 		<div id="Phase">
 		<h2>Choisir le plan d&apos;esquisse</h2>
-		<p>choisir un plan d&apos;esquisse (Face, Dessus ou Doite) dans l&apos;arbre de cr&eacute;ation<img src="/Vue/images/arbre.png" style="vertical-align:middle" alt="Arbre de cr&eacute;ation vide"></p>
+		<p>choisir un plan d&apos;esquisse (Face, Dessus ou Doite) dans l&apos;arbre de cr&eacute;ation<img src="/images/arbre.png" style="vertical-align:middle" alt="Arbre de cr&eacute;ation vide"></p>
 		<p>Vid&eacute;o de d&eacute;monstration &agrave; venir.</p>
 		</div>
 		<?php // <a href="Vue/planDesquisse.avi">Montre moi</a>
@@ -44,7 +44,7 @@ class PageVE extends PageArticle	{
 		<div id="Phase">
 		<h2>Esquisse cot&eacute;e</h2>
 		<p>Il faut dessiner :<img src="/Controleur/<?=$this->dossier?>/esquisse.png" style="vertical-align:middle; height:300px" alt="esquisse cot&eacute;e"></p>
-		<p>Dans la barre d&apos;outils, cliquez sur l&apos;alpha <b>Esquisse</b> (deuxi&egrave;me alpha) :<img src="/Vue/images/outilsEsquisse.png" alt="Barre d&apos;outils Esquisse"></p>
+		<p>Dans la barre d&apos;outils, cliquez sur l&apos;alpha <b>Esquisse</b> (deuxi&egrave;me alpha) :<img src="/images/outilsEsquisse.png" alt="Barre d&apos;outils Esquisse"></p>
 		<p>Vous aurez besoin des ic&ocirc;nes:</p>
 		<ul>
 		<li>
@@ -54,8 +54,8 @@ class PageVE extends PageArticle	{
 				echo ' et ',$icone_secondaire,'<img src="/Controleur/',$this->dossier,'/icone2.png" style="height:30px; vertical-align:middle" alt="ic&ocirc;ne ',$icone_secondaire,'">';
 		?>
 		</li>
-		<?=$extrusion ? '' : '<li>ligne de construction<img src="/Vue/images/ligne2construction.png" style="height:30px; vertical-align:middle" alt="ic&ocirc;ne ligne de construction"> pour cr&eacute;er l&aos;axe de r&eacute;volution.</li>'?>
-		<li>cotation intelligente<img src="/Vue/images/cotation.png" style="vertical-align:middle" alt="ic&ocirc;ne cotation intelligente">pour coter votre esquisse.</li>
+		<?=$extrusion ? '' : '<li>ligne de construction<img src="/images/ligne2construction.png" style="height:30px; vertical-align:middle" alt="ic&ocirc;ne ligne de construction"> pour cr&eacute;er l&aos;axe de r&eacute;volution.</li>'?>
+		<li>cotation intelligente<img src="/images/cotation.png" style="vertical-align:middle" alt="ic&ocirc;ne cotation intelligente">pour coter votre esquisse.</li>
 		</ul>
 		<p>Vid&eacute;o de d&eacute;monstration &agrave; venir.</p>
 		</div>
@@ -68,18 +68,18 @@ class PageVE extends PageArticle	{
 		<div id="Phase">
 		<h2>Fonction de mise en volume</h2>
 		<ol>
-		<li>Dans la barre d&apos;outils, s&eacute;lectionnez l&apos;alpha <b>Fonctions</b> (premier alpha) :<img src="/Vue/images/fonctions.png" style="vertical-align:middle" alt="Barre d&apos;outils Fonctions."></li>
+		<li>Dans la barre d&apos;outils, s&eacute;lectionnez l&apos;alpha <b>Fonctions</b> (premier alpha) :<img src="/images/fonctions.png" style="vertical-align:middle" alt="Barre d&apos;outils Fonctions."></li>
 		<li>Cliquez sur l&apos;ic&ocirc;ne <b><?=$extrusion ? 'Base/Bossage extrud&eacute;' : 'Base bossage avec r&eacute;volution'?></b>
-		<img src="/Vue/images/<?=$extrusion ? 'extrusion' : 'revolution'?>.png" style="height:30px; vertical-align:middle" alt="ic&ocirc;ne de mise en volume">
+		<img src="/images/<?=$extrusion ? 'extrusion' : 'revolution'?>.png" style="height:30px; vertical-align:middle" alt="ic&ocirc;ne de mise en volume">
 		<?=$extrusion ? ' premi&egrave;re' : ' deuxi&egrave;me'?> ic&ocirc;ne.</li>
 
-		<p class="gauche"><img src="/Vue/images/param_<?=$extrusion ? 'extrusion' : 'revolution'?>.png" alt="param&egrave;tres"></p>
+		<p class="gauche"><img src="/images/param_<?=$extrusion ? 'extrusion' : 'revolution'?>.png" alt="param&egrave;tres"></p>
 		<li style="margin-top:50px">A gauche de l&apos;&eacute;cran apparaissent les param&egrave;tres</li>
 		<li><?=$extrusion ? 'Dans la partie <b>Direction 1</b>, inscrivez la profondeur ici 70 mm' : 'Si la case <b>Axe de r&eacute;volution</b> n&apos;est renseign&eacute;e (ici ligne5) il faut sélectionner l&apos;axe de r&eacute;volution'?>.</li>
 		<?php if (($extrusion) && ($dépouille))
-			echo '<li>Cliquez sur l&apos;ic&ocirc;ne d&eacute;pouille <img src="/Vue/images/depouille.png" style="height:30px; vertical-align:middle" alt="icocirc:ne d&eacute;pouille"> puis entrez l&apos;angle en degr&eacute;s.</li>';
+			echo '<li>Cliquez sur l&apos;ic&ocirc;ne d&eacute;pouille <img src="/images/depouille.png" style="height:30px; vertical-align:middle" alt="icocirc:ne d&eacute;pouille"> puis entrez l&apos;angle en degr&eacute;s.</li>';
 		?>
-		<li>Enfin validez en cliquant sur <img src="/Vue/images/validation.png" style="height:30px; vertical-align:middle" alt="icocirc:ne d&eacute;pouille"> en haut à gauche.</li>
+		<li>Enfin validez en cliquant sur <img src="/images/validation.png" style="height:30px; vertical-align:middle" alt="icocirc:ne d&eacute;pouille"> en haut à gauche.</li>
 		</ol>
 		<p>Vid&eacute;o de d&eacute;monstration &agrave; venir.</p>
 		</div>
