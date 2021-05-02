ff0d93b9c8472add6122b3f24ed59a6a48923a78
ChristopHe <christophe.hervi@free.fr>
2021-05-02 04:21:18 +0200

    création de la requête donnant les liens internes sans dépendre d'une vue

--- PEUNC/BDD/Vue_pagesConnexes.sql
+++ PEUNC/BDD/Vue_pagesConnexes.sql
@@ -7,5 +7,12 @@ CREATE VIEW Vue_pagesConnexes AS
 	INNER JOIN Connexes ON Squelette.ID = Connexes.Squelette_ID
 UNION
 	#-- liens internes
-	SELECT alpha, beta, gamma, URL FROM Vue_pagesConnexesInternes
+	SELECT
+		Vue_URLvalides.niveau1 AS alpha,
+		Vue_URLvalides.niveau2 AS beta,
+		Vue_URLvalides.niveau3 AS gamma,
+		CONCAT('<a href="',Vue_URLvalidesBis.URL,'">',ConnexesInternes.texte,'</a>' ) AS URL
+	FROM ConnexesInternes
+	INNER JOIN  Vue_URLvalides ON Vue_URLvalides.ID = ConnexesInternes.squelette_ID
+	INNER JOIN  Vue_URLvalides AS Vue_URLvalidesBis ON Vue_URLvalidesBis.ID = ConnexesInternes.lien
 ORDER BY alpha ASC, beta ASC, gamma ASC
