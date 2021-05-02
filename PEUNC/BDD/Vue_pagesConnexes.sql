1eec3d27d10ae16b9a1ede011e577b2ddc6adadb
ChristopHe <christophe.hervi@free.fr>
2021-05-02 04:09:14 +0200

    intégration de la vue sur les liens externes dans la vue pages connexes

--- PEUNC/BDD/Vue_pagesConnexes.sql
+++ PEUNC/BDD/Vue_pagesConnexes.sql
@@ -1,7 +1,11 @@
 #-- regroupement des liens internes et externes
 DROP VIEW IF EXISTS Vue_pagesConnexes;
 CREATE VIEW Vue_pagesConnexes AS
-SELECT alpha, beta, gamma, URL FROM Vue_pagesConnexesExternes
+	#-- liens externes
+	SELECT	alpha, beta, gamma, CONCAT('<a href="',URL,'" target="_blank">',Connexes.texte,'</a>' ) AS URL
+	FROM Squelette
+	INNER JOIN Connexes ON Squelette.ID = Connexes.Squelette_ID
 UNION
-SELECT alpha, beta, gamma, URL FROM Vue_pagesConnexesInternes
+	#-- liens internes
+	SELECT alpha, beta, gamma, URL FROM Vue_pagesConnexesInternes
 ORDER BY alpha ASC, beta ASC, gamma ASC
