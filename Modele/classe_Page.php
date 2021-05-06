f57e5c49bb9345153ca41db413dedec544f4db2b
ChristopHe <christophe.hervi@free.fr>
2021-05-07 01:23:55 +0200

    utilisation de la méthode statique BaliseImage

--- Modele/classe_Page.php
+++ Modele/classe_Page.php
@@ -27,7 +27,7 @@ class PageVE extends PageArticle	{
 	public function TitreVE($titre, $VE) { // VE et dossierVE ne sont pas forcément identiques.Exemple: tronc de cône et tronc2cone
 		?>
 		<h1>Cr&eacute;er <?=$titre?> </h1>
-		<p>On veut r&eacute;aliser : <?=$this->BaliseImage("Piece/{$this->dossier}/VEcote.png","{$VE} cot&eacute;",'style="vertical-align:middle; height:300px"')?></p>
+		<p>On veut r&eacute;aliser : <?=\PEUNC\classes\Page::BaliseImage("Piece/{$this->dossier}/VEcote.png","{$VE} cot&eacute;",'style="vertical-align:middle; height:300px"')?></p>
 		<?php
 	}
 
@@ -35,7 +35,7 @@ class PageVE extends PageArticle	{
 		?>
 		<div id="Phase">
 		<h2>Choisir le plan d&apos;esquisse</h2>
-		<p>choisir un plan d&apos;esquisse (Face, Dessus ou Doite) dans l&apos;arbre de cr&eacute;ation<?=$this->BaliseImage("arbre.png","Arbre de cr&eacute;ation vide",'style="vertical-align:middle"')?></p>
+		<p>choisir un plan d&apos;esquisse (Face, Dessus ou Doite) dans l&apos;arbre de cr&eacute;ation<?=\PEUNC\classes\Page::BaliseImage("arbre.png","Arbre de cr&eacute;ation vide",'style="vertical-align:middle"')?></p>
 		<p>Vid&eacute;o de d&eacute;monstration &agrave; venir.</p>
 		</div>
 		<?php // <a href="Vue/planDesquisse.avi">Montre moi</a>
@@ -45,19 +45,19 @@ class PageVE extends PageArticle	{
 		?>
 		<div id="Phase">
 		<h2>Esquisse cot&eacute;e</h2>
-		<p>Il faut dessiner :<?=$this->BaliseImage("Piece/{$this->dossier}/esquisse.png","esquisse cot&eacute;e",'style="vertical-align:middle; height:300px"')?></p>
-		<p>Dans la barre d&apos;outils, cliquez sur l&apos;onglet <b>Esquisse</b> (deuxi&egrave;me onglet) :<?=$this->BaliseImage("outilsEsquisse.png","Barre )?d&apos;outils Esquisse")?></p>
+		<p>Il faut dessiner :<?=\PEUNC\classes\Page::BaliseImage("Piece/{$this->dossier}/esquisse.png","esquisse cot&eacute;e",'style="vertical-align:middle; height:300px"')?></p>
+		<p>Dans la barre d&apos;outils, cliquez sur l&apos;onglet <b>Esquisse</b> (deuxi&egrave;me onglet) :<?=\PEUNC\classes\Page::BaliseImage("outilsEsquisse.png","Barre )?d&apos;outils Esquisse")?></p>
 		<p>Vous aurez besoin des ic&ocirc;nes:</p>
 		<ul>
 			<li>
 				<?php
-				echo $icone_principale,$this->BaliseImage("Piece/{$this->dossier}/icone.png", "ic&ocirc;ne {$icone_principale}",'style="height:30px; vertical-align:middle"');
+				echo $icone_principale,\PEUNC\classes\Page::BaliseImage("Piece/{$this->dossier}/icone.png", "ic&ocirc;ne {$icone_principale}",'style="height:30px; vertical-align:middle"');
 				if ($icone_secondaire != '')
-					echo " et {$icone_secondaire}", $this->BaliseImage("Piece/{$this->dossier}/icone2.png","ic&ocirc;ne {$icone_secondaire}",'style="height:30px; vertical-align:middle"');
+					echo " et {$icone_secondaire}", \PEUNC\classes\Page::BaliseImage("Piece/{$this->dossier}/icone2.png","ic&ocirc;ne {$icone_secondaire}",'style="height:30px; vertical-align:middle"');
 				?>
 			</li>
-			<?=$extrusion ? '' : '<li>ligne de construction' . $this->BaliseImage("ligne2construction.png","ic&ocirc;ne ligne de construction",'style="height:30px; vertical-align:middle"') . "pour cr&eacute;er l&apos;axe de r&eacute;volution.</li>\n\t"?>
-			<li>cotation intelligente<?=$this->BaliseImage("cotation.png", "ic&ocirc;ne cotation intelligente",'style="vertical-align:middle"')?> . pour coter votre esquisse.</li>
+			<?=$extrusion ? '' : '<li>ligne de construction' . \PEUNC\classes\Page::BaliseImage("ligne2construction.png","ic&ocirc;ne ligne de construction",'style="height:30px; vertical-align:middle"') . "pour cr&eacute;er l&apos;axe de r&eacute;volution.</li>\n\t"?>
+			<li>cotation intelligente<?=\PEUNC\classes\Page::BaliseImage("cotation.png", "ic&ocirc;ne cotation intelligente",'style="vertical-align:middle"')?> . pour coter votre esquisse.</li>
 		</ul>
 		<p>Vid&eacute;o de d&eacute;monstration &agrave; venir.</p>
 		</div>
@@ -70,20 +70,20 @@ class PageVE extends PageArticle	{
 		<div id="Phase">
 		<h2>Fonction de mise en volume</h2>
 		<ol>
-			<li>Dans la barre d&apos;outils, s&eacute;lectionnez l&apos;alpha <b>Fonctions</b> (premier alpha) :<?=$this->BaliseImage("fonctions.png","Barre d&apos;outils Fonctions.",'style="vertical-align:middle"')?></li>
+			<li>Dans la barre d&apos;outils, s&eacute;lectionnez l&apos;alpha <b>Fonctions</b> (premier alpha) :<?=\PEUNC\classes\Page::BaliseImage("fonctions.png","Barre d&apos;outils Fonctions.",'style="vertical-align:middle"')?></li>
 			<li>Cliquez sur l&apos;ic&ocirc;ne <b><?=$extrusion ? 'Base/Bossage extrud&eacute;' : 'Base bossage avec r&eacute;volution'?></b>
-				<?=$this->BaliseImage(($extrusion ? 'extrusion.png' : 'revolution.png'),"ic&ocirc;ne de mise en volume",'style="height:30px; vertical-align:middle"')?>
+				<?=\PEUNC\classes\Page::BaliseImage(($extrusion ? 'extrusion.png' : 'revolution.png'),"ic&ocirc;ne de mise en volume",'style="height:30px; vertical-align:middle"')?>
 				<?=$extrusion ? ' premi&egrave;re' : ' deuxi&egrave;me'?> ic&ocirc;ne.
 			</li>
 
-			<p class="gauche"><?=$this->BaliseImage(($extrusion ? 'param_extrusion.png' : 'param_revolution.png'),"param&egrave;tres")?></p>
+			<p class="gauche"><?=\PEUNC\classes\Page::BaliseImage(($extrusion ? 'param_extrusion.png' : 'param_revolution.png'),"param&egrave;tres")?></p>
 
 			<li style="margin-top:50px">A gauche de l&apos;&eacute;cran apparaissent les param&egrave;tres</li>
 			<li><?=$extrusion ? 'Dans la partie <b>Direction 1</b>, inscrivez la profondeur ici 70 mm' : 'Si la case <b>Axe de r&eacute;volution</b> n&apos;est renseign&eacute;e (ici ligne5) il faut sélectionner l&apos;axe de r&eacute;volution'?>.</li>
 			<?php if (($extrusion) && ($dépouille))
-				echo "<li>Cliquez sur l&apos;ic&ocirc;ne d&eacute;pouille ", $this->BaliseImage("depouille.png","icocirc:ne d&eacute;pouille",'style="height:30px; vertical-align:middle"'), "puis entrez l&apos;angle en degr&eacute;s.</li>";
+				echo "<li>Cliquez sur l&apos;ic&ocirc;ne d&eacute;pouille ", \PEUNC\classes\Page::BaliseImage("depouille.png","icocirc:ne d&eacute;pouille",'style="height:30px; vertical-align:middle"'), "puis entrez l&apos;angle en degr&eacute;s.</li>";
 			?>
-			<li>Enfin validez en cliquant sur <?=$this->BaliseImage("validation.png","icocirc:ne d&eacute;pouille",'style="height:30px; vertical-align:middle"')?> en haut à gauche.</li>
+			<li>Enfin validez en cliquant sur <?=\PEUNC\classes\Page::BaliseImage("validation.png","icocirc:ne d&eacute;pouille",'style="height:30px; vertical-align:middle"')?> en haut à gauche.</li>
 		</ol>
 		<p>Vid&eacute;o de d&eacute;monstration &agrave; venir.</p>
 		</div>
