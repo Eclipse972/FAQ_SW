65428746794175f24b4dfadf8eb5d4275bbf6810
ChristopHe <christophe.hervi@free.fr>
2021-05-11 19:27:51 +0200

    modification de la méthode pour l'icone principale

--- public/Modele/classe_PageVE.php
+++ public/Modele/classe_PageVE.php
@@ -21,17 +21,9 @@ class PageVE extends PageArticle	{
 
 	public function setIconePrincipale($icone_principale) { $this->icone_principale = $icone_principale; }
 	public function getIconePrincipale() {
-		echo $this->icone_principale,
-			\PEUNC\classes\Page::BaliseImage(	$this->dossier . "icone.png",
-												"ic&ocirc;ne " . $this->icone_principale,
-												'style="height:30px; vertical-align:middle"');
-		echo ($this->dossier == "Piece/sphere/") ? " et d&eacute;couper" . \PEUNC\classes\Page::BaliseImage("Piece/sphere/icone2.png","ic&ocirc;ne d&eacute;couper",'style="height:30px; vertical-align:middle"') : '';
+		return $this->icone_principale;
 	}
 
 	public function setObtenuParRévolution($flag) { $this->parRévolution = $flag; }
 	public function getObtenuParRévolution() { return $this->parRévolution; }
-
-	public function ParamètresVolume() {
-		echo \PEUNC\classes\Page::BaliseImage(($this->parRévolution  ? 'param_revolution.png' : 'param_extrusion.png' ),"param&egrave;tres");
-	}
 }
