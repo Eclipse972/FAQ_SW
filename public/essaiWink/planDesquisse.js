114e86bd7a74bfd978ad107aea4f5b19fa8b966f
ChristopHe <christophe.hervi@free.fr>
2021-05-11 22:27:00 +0200

    1er essai d'import de projet wink Pour cet essai j'ai juste impoté les fichiers dans un dossier essaiWink sans les répartir suivant leur nature (js, mp4, css...) dans le code html j'ai juste rajouter le dossier essaiWink pour accéder à tous ces fichier

---
+++ public/essaiWink/planDesquisse.js
@@ -0,0 +1,14 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "266": [
+      { type: "gotoframe", rect: { x: 664, y: 432, width: 60, height: 24 }, target: 267 },
+    ],
+    "456": [
+      { type: "gotoframe", rect: { x: 363, y: 444, width: 60, height: 24 }, target: 2 },
+    ],
+  },
+};
