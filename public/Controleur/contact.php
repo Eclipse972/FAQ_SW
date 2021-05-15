f8802bda9f797c7e429111093d30f02626a17b03
ChristopHe <christophe.hervi@free.fr>
2021-05-16 00:29:58 +0200

    controleur de la page de contact

---
+++ public/Controleur/contact.php
@@ -0,0 +1,3 @@
+<?php
+	$this->setCSS(["https://fonts.googleapis.com/css?family=Quicksand:400,700&effect=outline",	"commun",	"formulaire"]);
+	$this->setView("contact.html");
--- public/Modele/classe_PageContact.php
+++ public/Modele/classe_PageContact.php
@@ -3,8 +3,6 @@ class PageContact extends Page {
 
 	public function __construct() {
 		parent::__construct();
-		$this->setCSS(["https://fonts.googleapis.com/css?family=Quicksand:400,700&effect=outline",	"commun",	"formulaire"]);
-		$this->setView("contact.html");
 		if (empty($_POST))	{ // préparation affichage du formulaire
 
 		} else {	// traitement du formulaire
