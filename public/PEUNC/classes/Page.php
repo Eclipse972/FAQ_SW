c7ff2713cd75f2cbe29ca10fd811b38c547a3ad9
ChristopHe <christophe.hervi@free.fr>
2021-05-16 01:00:46 +0200

    La classe Contact doit nécessairement hériter de la clase Page définie par l'utilisateur

--- public/PEUNC/classes/Page.php
+++ public/PEUNC/classes/Page.php
@@ -128,3 +128,20 @@ class Page implements iPage	{
 		return '<img src="' . $src . '" alt="' . $alt . '" ' . $code . '>';
 	}
 }
+
+class Contact extends Page {
+	public function __construct() {
+		parent::__construct();
+		if (empty($_POST))	{ // préparation affichage du formulaire
+
+		} else {	// traitement du formulaire
+
+		}
+	}
+
+	public function Afficher_validation()	{
+		for($i=0;$i<5;$i++)	echo "\n\t\t\t<li>critère</li>";
+		echo "\n";
+	}
+
+}
