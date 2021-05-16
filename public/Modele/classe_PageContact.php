adc7121ec0add0ec62116e0fd19f5f826864e1ff
ChristopHe <christophe.hervi@free.fr>
2021-05-16 17:55:57 +0200

    utilisation de la méthode magique __call

--- public/Modele/classe_PageContact.php
+++ public/Modele/classe_PageContact.php
@@ -9,8 +9,7 @@ class PageContact extends Page {
 		$this->OContact = new \PEUNC\classes\Contact;	// héritage multiple impossible en PHP
 	}
 
-	public function Afficher_validation()	{
-		$this->OContact->Afficher_validation();
+	public function __call($methode,$argument)	{		// permet l'accès aux méthodeS de La classe Erreur de PEUNC
+		return $this->OContact->$methode($argument[0]);	// il n'y a au plus qu'un seul paramètre
 	}
-
 }
