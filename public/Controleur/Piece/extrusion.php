0b8a6af03730aae913341010b40a11572b9a2777
ChristopHe <christophe.hervi@free.fr>
2021-06-08 12:17:14 +0200

    code html sous forme d'une chaine de caactères

--- public/Controleur/Piece/extrusion.php
+++ public/Controleur/Piece/extrusion.php
@@ -1,9 +1,2 @@
 <?php
-ob_start();	// début du code <section>
-?>
-	<h1>Extrusion</h1>
-	<p>Page en construction</p>
-<?php
-$tampon = ob_get_contents();
-ob_end_clean();
-$this->setSection($tampon);
+$this->setSection("<h1>Extrusion</h1>\n<p>Page en construction</p>\n");
