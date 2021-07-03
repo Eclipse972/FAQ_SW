f22d697b8eafe4d5ab3b7f6b03f5d5d8af5c9fcf
ChristopHe <christophe.hervi@free.fr>
2021-07-04 01:05:00 +0200

    utilisation du conteneur pour la classe page

--- public/Modele/classe_Page.php
+++ public/Modele/classe_Page.php
@@ -1,15 +1,23 @@
 <?php
-class Page extends PEUNC\classes\Page {
+class Page {
 	protected $logo;
+	protected $BD;
+	protected $conteneurPage;
 
 	public function __construct(array $TparamURL = []) {
-		parent::__construct($TparamURL);
+		$this->conteneurPage = new PEUNC\classes\Page($TparamURL);
+		$this->BD = new PEUNC\classes\BDD;
 		// valeurs par défaut
 		$this->setTitle("La Foire Aux Questions sur SolidWorks de ChristopHe");
 		$this->setHeaderText("<p class=\"font-effect-outline\">Foire Aux Questions SolidWorks de ChristopHe</p>");
 		$this->setLogo("logo.png");
 		$this->setFooter("");
 	}
+
+	public function __call($methode,$argument)	{		// permet l'accès aux méthodes du conteneur Page de PEUNC
+		return $this->conteneurPage->$methode($argument[0]);	// il n'y a au plus qu'un seul paramètre
+	}
+
 // Méthodes pour la nouvelle variable-membre logo
 	public function setLogo($logo) {	// nom de la forme /sous/dossier/fichier.extension à partir du dossier image du site
 		$this->logo = $logo;
