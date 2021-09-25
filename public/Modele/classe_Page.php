feb0113b12390295f72af1222097b68f8f7e25ab
ChristopHe <christophe.hervi@free.fr>
2021-09-25 04:28:41 +0200

    Fin du délire Je me passerai de l'héritage multiple; la classe Page de l'utilisateur est une fille de la classe page de PEUNC

--- public/Modele/classe_Page.php
+++ public/Modele/classe_Page.php
@@ -1,21 +1,14 @@
 <?php
-class Page {
+class Page extends PEUNC\classes\Page {
 	protected $BD;
-	protected $conteneurPage;
 
 	public function __construct(array $TparamURL = []) {
-		$this->conteneurPage = new PEUNC\classes\Page($TparamURL);
 		$this->BD = new PEUNC\classes\BDD;
 		// valeurs par défaut
 		$this->setTitle("La Foire Aux Questions sur SolidWorks de ChristopHe");
 		$this->setHeaderText("<p class=\"font-effect-outline\">Foire Aux Questions SolidWorks de ChristopHe</p>");
 		$this->setLogo("logo.png");
 		$this->setFooter("");
-	}
-
-	public function __call($methode,$argument)	{		// permet l'accès aux méthodes du conteneur Page de PEUNC
-		return $this->conteneurPage->$methode($argument[0], $argument[1], $argument[2]);
-		// il y a au plus trois paramètres. Solution "sale" mais qui fonctionne
 	}
 
  /* ***************************
@@ -29,17 +22,5 @@ class Page {
 				echo "\t<li>", (($alpha == $_SESSION['alpha']) ? str_replace('href', 'id="alpha_actif" href', $code) : $code), "</li>\n";
 		}
 		echo "\t</ul>\n";
-	}
-
-	public function ExecuteControleur($alpha, $beta, $gamma)	{
-		$script = $this->conteneurPage->CheminControleur($alpha, $beta, $gamma);
-		$dossierControleur = PEUNC\classes\Page::DOSSIER_CONTROLEUR;
-		if($script == '')
-			throw new Exception("Controleur non d&eacute;fini");
-		else {
-			if (file_exists($dossierControleur. $script))
-				require($dossierControleur . $script);
-			else throw new Exception("Controleur inexistant");
-		}
 	}
 }
