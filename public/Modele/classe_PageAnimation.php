286731166f98ca7de02bbcc1a74bd77d3642dcd1
Christophe HERVI <christophe.hervi@free.fr>
2022-12-04 01:50:54 +0100

    création de la classe de page pour les animations

--- public/Modele/classe_PageAnimation.php
+++ public/Modele/classe_PageAnimation.php
@@ -1,4 +1,11 @@
 <?php
 class PageAnimation extends PageArticle
 {
+	public function __construct(PEUNC\HttpRoute $route = null, array $TparamURL = [])
+	{
+		parent::__construct($route, $TparamURL);
+		$this->setCSS("animation");
+		$this->setView("pageAnimation.html");
+	}
+
 }
