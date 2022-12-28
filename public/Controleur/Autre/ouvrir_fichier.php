cbb0443cb13d3d81df649388633b0f6e05ddc86c
Christophe HERVI <christophe.hervi@free.fr>
2022-12-28 19:08:28 +0100

    corrections dans le texte

--- public/Controleur/Autre/ouvrir_fichier.php
+++ public/Controleur/Autre/ouvrir_fichier.php
@@ -2,9 +2,10 @@
 ob_start();	// début du code <section>
 ?>
 	<h1>Ouvrir efficacement un fichier</h1>
-	<p>Un projet peut contenir un nombre &eacute;lev&eacute; de pi&egrave;ces. Un moyen de s&apos;en sortir est de filtrer la liste de fichier.</p>
+	<p>Un projet peut contenir un nombre &eacute;lev&eacute; de fichiers. Un moyen de s&apos;en sortir est de filtrer la liste de fichiers.</p>
 	<ol>
-		<li>cliquez dans le menu "Fichier/Ouvrir" ou sur l&apos;ic&ocirc;ne ouvrir</li>
+		<li>cliquez dans le menu "Fichier/Ouvrir" ou sur l&apos;ic&ocirc;ne ouvrir <b>icone à venir</b>
+			<br><b>image de la boite de dialogue à venir</b></li>
 		<li>Dans la oite de dialogue en bas à droite sékection le type de fichier: Pi&egrave;ce pour ne s&eacute;lectionner que les pi&egrave;ces.</li>
 		<li>La liste des fichiers diminue dans la zone. S&eacute;lectionnez le fichier puis cliquez sur le bouton ouvrir.
 			<br>Remarque:<br>Pour allez plus vite on peut double-cliquer sur le fichier</li>
