a35fe7bef2547178356841b24ede79deb4c64239
Christophe HERVI <christophe.hervi@free.fr>
2023-01-06 14:21:25 +0100

    création de l'article cotation

--- public/Controleur/MEP/coter.php
+++ public/Controleur/MEP/coter.php
@@ -1,8 +1,16 @@
 <?php
-ob_start();	// début du code <section>
+ob_start();	// d&eacute;but du code <section>
 ?>
 	<h1>Coter un dessin</h1>
-	<p>La cotation concerne surtout les dessins de d&eacute;finition. C&apos;est &agrave; dire le dessin d'une pi&egrave;ce seule. Si la pi&egrave;ce &agrave; &eacute;t&eacute; r&eacute;alis&eacute;e correctement, elle contient la cotation.</p>
-	<p>Pour un dessin d'ensemble, il peut être n&eacute;cessaire de rajouter quelques cotes comme les ajustement les d&eacute;placements de pi&egrave;ce.</p>
+	<p>La cotation concerne surtout les dessins de d&eacute;finition. C&apos;est &agrave; dire le dessin d'une pi&egrave;ce seule. Si la pi&egrave;ce &agrave; &eacute;t&eacute; r&eacute;alis&eacute;e correctement, elle contient sa cotation.</p>
+	<ol>
+	<li>dans la barre d&apos;outils cliquez sur l&apos;onglet <b>Annotation</b></li>
+	<li>dans la barre d&apos;outils, cliquez sur <b>Objets du modèle</b></li>
+	<?=PEUNC\Page::BaliseImage("MEP/objetDuModele.png")?>
+	<li>à gauche de l&apos;&eacute;cran apparaîsssent les paramètres. Dans la partie Source/Destination cliquez sur Fonction s&eacute;lectionn&eacute;e (c'est une liste) et remplacer par Modèle entier.</li>
+	<?=PEUNC\Page::BaliseImage("MEP/source_modeleEntier.png")?>
+	<li>Enfin validez en cliquant sur <?=PEUNC\Page::BaliseImage("validation.png","icocirc:ne d&eacute;pouille",'style="height:30px; vertical-align:middle"')?> en haut à gauche.</li>
+	</ol>
+	<p><a href=/Animations/coterDessin>Montre moi comment ajouter la cotation d'un pièce sur une mise en plan</a></p>
 <?php
 $this->setSection(ob_get_clean());
