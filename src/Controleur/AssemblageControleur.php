df90f122e32d14ec4a96d0315a40f0f90c027a39
Christophe HERVI <christophe.hervi@free.fr>
2026-07-14 07:12:14 +0200

    création des méthode pour AssemblageControleur

--- src/Controleur/AssemblageControleur.php
+++ src/Controleur/AssemblageControleur.php
@@ -19,4 +19,213 @@ class AssemblageControleur extends ArticleControleur
 		$this->hydrate('assemblage');
 	}
 
+	/**
+	 * Accueil assemblage.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage
+	 */
+	public function accueil(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'accueil');
+	}
+
+	/**
+	 * Menu des contraintes.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/contraintes
+	 */
+	public function contraintes(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'contraintes');
+	}
+
+	/**
+	 * Contrainte d'appui plan.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/contraintes/appui-plan
+	 */
+	public function appuiPlan(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'appui-plan');
+	}
+
+	/**
+	 * Contrainte de coaxialité.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/contraintes/coaxialite
+	 */
+	public function coaxialite(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'coaxialite');
+	}
+
+	/**
+	 * Contraintes limites.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/contraintes/contraintes-limites
+	 */
+	public function contraintesLimites(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'contraintes-limites');
+	}
+
+	/**
+	 * Comment choisir ses contraintes.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/contraintes/comment-les-choisir
+	 */
+	public function commentLesChoisir(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'comment-les-choisir');
+	}
+
+	/**
+	 * Menu arbre de création.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/arbre-de-creation
+	 */
+	public function arbreDeCreation(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'arbre-de-creation');
+	}
+
+	/**
+	 * Liaison arbre vers zone graphique.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/arbre-de-creation/arbre-zone-graphique
+	 */
+	public function liaisonArbreZoneGraphique(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'arbre-zone-graphique');
+	}
+
+	/**
+	 * Liaison zone graphique vers arbre.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/arbre-de-creation/zone-graphique-arbre
+	 */
+	public function LiaisonZoneGraphiqueArbre(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'zone-graphique-arbre');
+	}
+
+	/**
+	 * Ouvrir un sous-ensemble.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/arbre-de-creation/ouvrir-sous-ensemble
+	 */
+	public function ouvrirSousEnsemble(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'ouvrir-sous-ensemble');
+	}
+
+	/**
+	 * Cacher ou montrer un composant.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/arbre-de-creation/cacher-montrer-composant
+	 */
+	public function cacherMontrerComposant(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'cacher-montrer-composant');
+	}
+
+	/**
+	 * Voir les contraintes.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/arbre-de-creation/voir-les-contraintes
+	 */
+	public function voirLesContraintes(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'voir-les-contraintes');
+	}
+
+	/**
+	 * Les configurations.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/configurations
+	 */
+	public function configurations(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'configurations');
+	}
+
+	/**
+	 * Créer un éclaté.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/creer-un-eclate
+	 */
+	public function eclate(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'creer-un-eclate');
+	}
+
+	/**
+	 * Créer un écorché.
+	 *
+	 * @param Request $requete
+	 * @param Response $reponse
+	 * @return Response
+	 *
+	 * @route /assemblage/creer-un-ecorche
+	 */
+	public function ecorche(Request $requete, Response $reponse): Response
+	{
+		return $this->renduPageOrdinaire($reponse, 'creer-un-ecorche');
+	}
 }
